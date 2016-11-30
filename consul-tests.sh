4edc6207639d429b40c5e8f36d534c1d88260c18
Gyu-Ho Lee <gyuhox@gmail.com>
2016-11-30 00:40:36 -0800

    *: new tests

--- consul-tests.sh
+++ consul-tests.sh
@@ -5,4 +5,5 @@ echo "Installing consul-tests..."
 go install -v
 
 echo "Starting tests..."
-consul-tests -clients 1000 -endpoints localhost:8500 -key-size 8 -val-size 256 -writes 1000000
+consul-tests -clients 1000 -endpoints localhost:8500 -key-size 8 -val-size 256 -writes 2000000
+
